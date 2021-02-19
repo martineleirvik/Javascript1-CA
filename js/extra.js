743dedc1fd1960ccdee0178c402700b362aa1e52
martineleirvik <leirvikmartine@gmail.com>
2021-02-19 21:58:51 +0100

    extra

--- js/extra.js
+++ js/extra.js
@@ -1,32 +1,42 @@
-const detailContainer = document.querySelector(".joke-detail");
-
 const url = "http://api.icndb.com/jokes";
 const proxy = "https://noroffcors.herokuapp.com/";
 
 const corsFix = proxy + url;
 
-async function fetchJoke() {
+const resultsContainer = document.querySelector(".Thejoke");
+
+
+async function getJokes() {
 
     try {
 
-        const response = await fetch(corsFix);
-        const content = await response.json();
+    const response = await fetch(corsFix);
 
-        console.log(content);
+    const content = await response.json();
 
-        createHtml(content);
-    
+    console.log(content);
+
+    const results = content.value;
+
+    resultsContainer.innerHTML = "";
+
+    for(let i = 0; i < results.length; i++) {
+        console.log(results[i].joke);
+
+        
+        if (i === 5) {
+            break;
+        }
+
+        resultsContainer.innerHTML += `<a href="details.html?id=${results.id}" class="jokeresult"> ${results[i].joke} </a>`;
+    }
     }
 
     catch (error) {
         console.log(error);
-        detailContainer.innerHTML = displayError("An error occured")
+        resultsContainer.innerHTML = displayError("An error occured")
     }
 }
 
-fetchJoke();
 
-function createHtml(content) {
-    detailContainer.innerHTML = `<h1> ${Joke} </h1>
-                                <div class="detailjoke" ${results[i].joke} </div>`;
-}
+getJokes();
